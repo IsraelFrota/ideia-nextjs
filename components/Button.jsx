53304715aca6fc75d7420d42a23f5b1e0f0ca140
Ricardo Martins <desenvolvedor01.israel@gmail.com>
2024-12-23 16:16:44 -0300

    Adicionando uma flag para poder desabilitar o botão

--- components/Button.jsx
+++ components/Button.jsx
@@ -1,11 +1,12 @@
 import style from "../style/style.module.css";
 
-function Button({ label, handleChange }) {
+function Button({ label, handleChange, disabled }) {
 	return (
 		<button
 			className={style.button_style}
 			type="button"
 			onClick={handleChange}
+			disabled={disabled}
 		>
 			{label}
 		</button>
