3e3fedbcd9f80354f7340be8d0a4f9f8660162f8
Ricardo Martins <desenvolvedor01.israel@gmail.com>
2024-12-31 09:40:30 -0300

    Aplicando uma aleatóriedade ao status

--- components/report/Result.jsx
+++ components/report/Result.jsx
@@ -1,19 +1,45 @@
 import { useEffect, useState } from "react";
 
+import status from "../../lib/status.json";
+
 import style from "../../style/style.module.css";
 
-function Result({ data }) {
+function Result({ data, option }) {
 	const { score } = data.result;
 	const [result, setResult] = useState('');
 
+	function getRandomIntInclusive(min, max) {
+		min = Math.ceil(min);
+		max = Math.floor(max);
+		return Math.floor(Math.random() * (max - min + 1)) + min;
+	};
+
 	useEffect(() => {
 		const resultMap = {
-			100: "Excelente!",
-			80: "Bom mas pode melhorar.",
-			default: "Não está nada bom."
+			100: (option === 1) ? 
+				status.funny.excellent[getRandomIntInclusive(0, 2)] : 
+				(option === 2) ? 
+				status.neutral.excellent[getRandomIntInclusive(0, 2)] : 
+				status.formal.excellent[getRandomIntInclusive(0, 2)],
+			80: (option === 1) ? 
+				status.funny.potentialToImprove[getRandomIntInclusive(0, 2)] : 
+				(option === 2) ? 
+				status.neutral.potentialToImprove[getRandomIntInclusive(0, 2)] : 
+				status.formal.potentialToImprove[getRandomIntInclusive(0, 2)],
+			default: (option === 1) ? 
+				status.funny.notGood[getRandomIntInclusive(0, 2)] : 
+				(option === 2) ? 
+				status.neutral.notGood[getRandomIntInclusive(0, 2)] : 
+				status.formal.notGood[getRandomIntInclusive(0, 2)]
 		};
 	
-		setResult(score === 100 ? resultMap[100] : score >= 80 ? resultMap[80] : resultMap.default);
+		setResult(
+			score === 100 ? 
+			resultMap[100] : 
+			score >= 80 ? 
+			resultMap[80] : 
+			resultMap.default
+		);
 	}, [score]);
 
 	return (
